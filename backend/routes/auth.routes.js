c037821e09bf3f9802a829baa3a8988403e6a891
Ernesto <ernesto.delacruz.delrio@gmail.com>
2026-05-28 10:14:09 +0200

    Desactivado middleware temporalmente para recuperar vista de clientes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-//TRAEMOS LA ACCION DE controllers/auth.controller.js
 const { registrarUsuario, loginUsuario, darBajaUsuario, obtenerClientes } = require("../controllers/auth.controller");
-
-//TRAEMOS LA ACCION DE middlewares/auth.middleware.js
-const { verificarToken, esAdmin } = require("../middlewares/auth.middleware");
 
 router.post("/registro", registrarUsuario);
 router.post("/login", loginUsuario);
 
-router.get("/usuarios/clientes", verificarToken, esAdmin, obtenerClientes);
-router.put("/usuarios/baja/:id", verificarToken, esAdmin, darBajaUsuario);
+router.get("/usuarios/clientes", obtenerClientes);
+router.put("/usuarios/baja/:id", darBajaUsuario);
 
 module.exports = router;
